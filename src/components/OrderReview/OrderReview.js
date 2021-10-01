b5134a0a5c325e9000b8c467e2f657141f7d7f48
rasel <russelroy15@gmail.com>
2021-10-01 23:41:58 +0600

    WIP on ema jon React router project

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,6 +1,7 @@
 import React from "react";
 import useCart from "../../hooks/useCart";
 import useProducts from "../../hooks/useProduct";
+import Cart from "../Cart/Cart";
 
 const OrderReview = () => {
   const [products] = useProducts();
@@ -10,6 +11,7 @@ const OrderReview = () => {
       <h1>{products.length}</h1>
       <h3>{cart.length}</h3>
       <h2>This is Order Review</h2>
+      <Cart cart={cart}></Cart>
     </div>
   );
 };
