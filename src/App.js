0794cb91667d58e4b8dd0b2150d0730f584efcbc
rasel <russelroy15@gmail.com>
2021-10-01 22:47:03 +0600

    WIP on ema jon React router project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Inventory from "./components/Inventory/Inventory";
+import NotFound from "./components/NotFound/NotFound";
+import OrderReview from "./components/OrderReview/OrderReview";
 import Shop from "./components/Shop/Shop";
 
 function App() {
@@ -14,6 +17,15 @@ function App() {
           </Route>
           <Route path="/shop">
             <Shop></Shop>
+          </Route>
+          <Route path="/review">
+            <OrderReview />
+          </Route>
+          <Route path="/inventory">
+            <Inventory />
+          </Route>
+          <Route path="*">
+            <NotFound />
           </Route>
         </Switch>
       </Router>
