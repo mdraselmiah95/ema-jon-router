be0aa005de933a954fa0dc39c35132a43df6eb3b
rasel <russelroy15@gmail.com>
2021-10-02 00:12:20 +0600

    WIP on ema jon React router project

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,14 +1,17 @@
 import React from "react";
 
 const ReviewItem = (props) => {
-  const { name, price, quantity } = props.product;
+  const { name, price, quantity, key } = props.product;
+  const { handleRemove } = props;
   return (
     <div className="product">
       <div>
         <h4 className="product-name">{name}</h4>
         <p>Price: {price}</p>
         <p> Quantity:{quantity}</p>
-        <button className="btn-regular">Remove Item</button>
+        <button onClick={() => handleRemove(key)} className="btn-regular">
+          Remove Item
+        </button>
       </div>
     </div>
   );
