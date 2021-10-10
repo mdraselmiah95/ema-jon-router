d576c6bf7cfd4f8cafa7cb7d9446271a645c7ec1
rasel <russelroy15@gmail.com>
2021-10-10 16:08:00 +0600

    WIP on ema john simple project

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
@@ -8,7 +9,19 @@ const Register = () => {
         <form onSubmit="">
           <input type="email" name="" id="" placeholder="Your Email" /> <br />
           <input type="password" name="" id="" placeholder="Your Password" />
+          <br />
+          <input
+            type="password"
+            name=""
+            id=""
+            placeholder="Re-enter Password"
+          />
+          <br />
+          <input type="submit" value="Submit" />
         </form>
+        <p>
+          ALready have an account ? <Link to="/login">Login</Link>
+        </p>
       </div>
     </div>
   );
