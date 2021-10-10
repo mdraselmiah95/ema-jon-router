bccfdc0cdb4fa7c0f706d6ab007462532baae147
rasel <russelroy15@gmail.com>
2021-10-10 15:40:07 +0600

    WIP on ema john simple project

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCnJOo8DZWC9TBrkmMSLwLKq3LCqbsBhHo",
+  authDomain: "ema-john-simple-ac815.firebaseapp.com",
+  projectId: "ema-john-simple-ac815",
+  storageBucket: "ema-john-simple-ac815.appspot.com",
+  messagingSenderId: "963540668817",
+  appId: "1:963540668817:web:213a23808aeb7d18f00a30",
+};
+export default firebaseConfig;
