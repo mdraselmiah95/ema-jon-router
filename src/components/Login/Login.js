a5e9995a0ae423489462de9680ef842d2656a1ca
rasel <russelroy15@gmail.com>
2021-10-10 15:54:06 +0600

    WIP on ema john simple project

---
+++ src/components/Login/Login.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <div>
+        <h2>Login</h2>
+        <form onSubmit="">
+          <input type="email" name="" id="" placeholder="Your Email" />
+          <br />
+          <input type="password" name="" id="" />
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
