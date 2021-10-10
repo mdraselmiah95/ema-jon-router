65a2a5280363e2d8343b72cfa90a0971488049a7
rasel <russelroy15@gmail.com>
2021-10-10 16:04:52 +0600

    WIP on ema john simple project

---
+++ src/components/Register/Register.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div>
+      <div>
+        <h2>Register: Create Account</h2>
+        <form onSubmit="">
+          <input type="email" name="" id="" placeholder="Your Email" /> <br />
+          <input type="password" name="" id="" placeholder="Your Password" />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
