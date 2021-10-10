1c264b873db17dc3c27cc8e9714321a2ecdf944b
rasel <russelroy15@gmail.com>
2021-10-10 17:28:23 +0600

    WIP on ema john simple project

---
+++ src/context/AuthProvider.js
@@ -0,0 +1,9 @@
+import React, { createContext } from "react";
+
+const AuthContext = createContext();
+const AuthProvider = ({ children }) => {
+  // const { children } = props;
+  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+};
+
+export default AuthProvider;
