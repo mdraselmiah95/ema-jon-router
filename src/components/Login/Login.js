050143ab3336ec9e26b9cebe5025c569722282cd
rasel <russelroy15@gmail.com>
2021-10-10 16:00:05 +0600

    WIP on ema john simple project

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
+    <div className="login-form">
       <div>
         <h2>Login</h2>
         <form onSubmit="">
